c5c28f12df7795b873a1b2bbdffa61c6e7b83403
ShayanKh76 <shayan.khalighi76@gmail.com>
2023-05-14 21:14:48 +0330

    feat: add date filter for events

--- src/app/Events/Events.tsx
+++ src/app/Events/Events.tsx
@@ -3,7 +3,8 @@ import { useQuery, gql } from "@apollo/client";
 import Event from "./components/Event";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faFilter, faSearch } from "@fortawesome/free-solid-svg-icons";
+import DateFilter from "./components/DateFilter";
 
 export type Conferences = {
   id: string;
@@ -90,6 +91,12 @@ const GET_DATA = gql`
 export default function Events() {
   const { loading, error, data } = useQuery(GET_DATA);
   const [search, setSearch] = useState("");
+  const [showFilter, setShowFilter] = useState(false);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [{ startDate, endDate }, setDate] = useState({
+    startDate: null,
+    endDate: null,
+  });
   const filteredEvents =
     data &&
     data.conferences.filter((event: any) =>
@@ -98,11 +105,43 @@ export default function Events() {
   const handleSearchInputChange = (event: any) => {
     setSearch(event.target.value);
   };
+
+  const handleFilter = (startDate: any, endDate: any) => {
+    const filteredData =
+      data &&
+      data.conferences.filter((item: Conferences) => {
+        const itemDate = new Date(item.startDate);
+        if (startDate && endDate) {
+          return (
+            itemDate >= new Date(startDate) && itemDate <= new Date(endDate)
+          );
+        } else if (startDate && !endDate) {
+          return itemDate >= new Date(startDate);
+        } else if (!startDate && endDate) {
+          return itemDate <= new Date(endDate);
+        } else {
+          return null;
+        }
+      });
+    setDate({ startDate, endDate });
+    setFilteredItems(filteredData);
+  };
+
+  const handleToggleFilter = () => {
+    setShowFilter(!showFilter);
+  };
   return (
     <>
       {loading && (
         <div className="fixed inset-0 flex items-center justify-center bg-white">
           <div className="animate-spin rounded-full h-16 w-16 border-t-2  border-blue-900"></div>
+        </div>
+      )}
+
+      {showFilter && (
+        <div className="absolute right-2 top-40 p-3 mt-1 border rounded-md bg-white w-72">
+          {/* Date filter component */}
+          <DateFilter onFilter={handleFilter} />
         </div>
       )}
 
@@ -113,28 +152,48 @@ export default function Events() {
           onChange={handleSearchInputChange}
           placeholder="Search Event"
           className="border rounded w-1/3 p-3 h-8"
-        />
+        />{" "}
+        <button
+          onClick={handleToggleFilter}
+          className=" absolute right-4 text-lg text-blue-500"
+        >
+          <FontAwesomeIcon icon={faFilter} className="mx-2" />
+        </button>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-8 m-4">
-        {search && filteredEvents
-          ? filteredEvents.map((item: Conferences, index: number) => {
-              return (
-                <div key={index}>
-                  <Event confrence={item} />
-                </div>
-              );
-            })
-          : data
-          ? data.conferences.map((item: Conferences, index: number) => {
-              return (
-                <div key={index}>
-                  <Event confrence={item} />
-                </div>
-              );
-            })
-          : null}
-      </div>
+      {(startDate || endDate) && filteredItems.length == 0 ? (
+        <div className="flex justify-center mt-4">
+          <span> No Event Found</span>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 m-4">
+          {search && filteredEvents
+            ? filteredEvents.map((item: Conferences, index: number) => {
+                return (
+                  <div key={index}>
+                    <Event confrence={item} />
+                  </div>
+                );
+              })
+            : filteredItems.length != 0
+            ? filteredItems.map((item: Conferences, index: number) => {
+                return (
+                  <div key={index}>
+                    <Event confrence={item} />
+                  </div>
+                );
+              })
+            : data
+            ? data.conferences.map((item: Conferences, index: number) => {
+                return (
+                  <div key={index}>
+                    <Event confrence={item} />
+                  </div>
+                );
+              })
+            : null}
+        </div>
+      )}
     </>
   );
 }
---
+++ src/app/Events/components/DateFilter.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function DateFilter({ onFilter }: any) {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const handleFilter = () => {
+    // Perform filtering logic based on startDate and endDate
+    onFilter(startDate, endDate);
+  };
+  const handleClear = () => {
+    setStartDate("");
+    setEndDate("");
+    onFilter();
+  };
+
+  return (
+    <div className="grid  h-30">
+      <label>Start Date:</label>
+      <input
+        type="date"
+        value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}
+        className="border rounded  p-3 h-8 my-2"
+      />
+      <label>End Date:</label>
+      <input
+        type="date"
+        value={endDate}
+        onChange={(e) => setEndDate(e.target.value)}
+        className="border rounded  p-3 h-8 my-2"
+      />
+      <button
+        onClick={handleFilter}
+        className=" h-7  bg-blue-400 rounded text-white"
+      >
+        Filter
+      </button>
+      <button
+        onClick={handleClear}
+        className=" h-7 border  border-blue-400 rounded text-blue-400 mt-2"
+      >
+        clear
+      </button>
+    </div>
+  );
+}
